210dc0536b13ef0d05563a3fe8c0c446c5a3a1cd
Jmjimmy20 <jmjimmy20@gmail.com>
2020-01-30 17:25:21 -0500

    adicion de web

--- src/main/java/edu/escuelaing/arem/designprimer/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/designprimer/SparkWebApp.java
@@ -1,12 +1,18 @@
 package edu.escuelaing.arem.designprimer;
 
+import spark.Request;
+import spark.Response;
 import static spark.Spark.*;
+
+import edu.escuelaing.arem.math.LinkedList;
+import edu.escuelaing.arem.math.;
 
 public class SparkWebApp{
 
     public static void main(String[] args){
         port(getPort());
-        get("/hello", (req, res) -> "Hello Heroku");
+        // get("/hello", (req, res) -> "Hello Heroku");
+        get("/calc", (req, res) -> mainPage(req, res));
     }
 
     static int getPort() {
@@ -14,5 +20,21 @@ public class SparkWebApp{
            return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567; //returns default port if heroku-port isn't set(i.e. on localhost) 
-       }
+    }
+
+    private static  String mainPage(Request req, Response res){
+        String content = "<!DOCTYPE html>" + "<html>" + "<body>"
+                + "<center>"
+                + "<h1>Calculadora</h1>"
+                + "<form action=\"/calc\">" 
+                + "Neste site, use o laboratório 1 para mostrar os resultados"
+                + "<h2>Inserir dados</h2>"
+                + "  <input type=\"text\" name=\"data\" placeholder=\"Ej. 1,2,3\">" + "<br/><br/>"
+                + "  <input type=\"submit\" value=\"Submit\">" + "</form>"
+                + "</center>"
+                + "</body>" + "</html>";
+        return content;
+    }
+
+    
 }
